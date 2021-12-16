be6b7ccf3b19e1006707544170e6badf20169bf9
Andrew Phillips <56562815+amp-tstc-fbccys@users.noreply.github.com>
2021-12-16 10:04:27 -0600

    Update provision_wazuh.sh
    added functionality to solicit and include wazuh authentication password

--- provision_wazuh.sh
+++ provision_wazuh.sh
@@ -3,7 +3,9 @@ cp /var/ossec/etc/ossec.conf /var/ossec/etc/ossec.conf.bak
 sed s/MANAGER_IP/'172.17.220.69'/ /var/ossec/etc/ossec.conf > /var/ossec/etc/ossec.conf.cys
 cp /var/ossec/etc/ossec.conf.cys /var/ossec/etc/ossec.conf
 cd /var/ossec/bin
-./agent-auth -m 172.17.220.69
+echo "Enter the Wazuh password"
+read password
+./agent-auth -m 172.17.220.69 -P $password
 systemctl daemon-reload
 systemctl enable wazuh-agent
 systemctl start wazuh-agent
